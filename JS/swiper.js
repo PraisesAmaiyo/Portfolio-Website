ab3785217fc208ba5497a6ca54c9fb8b7bc53031
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-02-28 14:16:00 +0100

    Updates added

--- JS/swiper.js
+++ JS/swiper.js
@@ -14,49 +14,6 @@ var swiper = new Swiper('.swiper-1', {
   },
 });
 
-// Testimonial
-
-// var swiper = new Swiper('.swiper-2', {
-//   slidesPerView: '2',
-//   spaceBetween: 30,
-//   freeMode: true,
-//   centeredSlides: true,
-//   loop: true,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-//   autoplay: {
-//     delay: 1000,
-//     disableOnInteraction: false,
-//   },
-// });
-
-// var swiper = new Swiper('.swiper-2', {
-//   effect: 'coverflow',
-//   grabCursor: true,
-//   centeredSlides: true,
-//   slidesPerView: 'auto',
-//   coverflowEffect: {
-//     rotate: 50,
-//     stretch: 0,
-//     depth: 100,
-//     modifier: 1,
-//     slideShadows: true,
-//   },
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-//   autoplay: {
-//     delay: 1000,
-//     disableOnInteraction: false,
-//   },
-// });
-
 // graphics design
 var swiper = new Swiper('.swiper-2', {
   effect: 'coverflow',
@@ -78,4 +35,61 @@ var swiper = new Swiper('.swiper-2', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  //   autoplay: {
+  //     delay: 1000,
+  //     disableOnInteraction: false,
+  //   },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+  },
 });
+
+// Testimonial;
+var swiper = new Swiper('.swiper-3', {
+  slidesPerView: '2',
+  spaceBetween: 30,
+  freeMode: true,
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+// var swiper = new Swiper('.swiper-2', {
+//   effect: 'coverflow',
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: 'auto',
+//   coverflowEffect: {
+//     rotate: 50,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+//   autoplay: {
+//     delay: 1000,
+//     disableOnInteraction: false,
+//   },
+// });
