14e36e97defc8fe56741944736f451b980a8ab21
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-03-07 00:17:57 +0100

    Contact form trial

--- sendEmail.php
+++ sendEmail.php
@@ -24,14 +24,14 @@
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$subject = $_POST['subject'];
+// $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "example@gmail.com";
+$recipient = "amaiyo.praises@gmail.com";
 
-mail($recipient, $subject, $message, $mailheader) or die("Error!");
+mail($recipient, $message, $mailheader) or die("Error!");
 
 echo'
 
